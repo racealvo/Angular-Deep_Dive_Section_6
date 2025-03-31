6ceb71310a2138b9b7c80eaed8694942c7fd8543
Ray Salvo <salvo.raymond@gmail.com>
2025-03-31 06:41:44 -0400

    Added ngOnInit hook to code which changes the class attribute randomly. This affects the label text content and style.

--- 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+      this.currentStatus = this.currentStatus === 'online' ? 'offline' : 'online';
+      console.log('Server status changed to:', this.currentStatus);
+    }, 5000);
+  }
 }
